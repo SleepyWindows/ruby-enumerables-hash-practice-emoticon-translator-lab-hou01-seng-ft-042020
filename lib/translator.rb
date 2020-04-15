9975878cc09ae05ff028690080dff6788c3879f9
edwin calvillo <calvillo.edwin@gmail.com>
2020-04-15 22:26:46 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,10 +2,19 @@ require 'yaml'
 
 def load_library (file)
   emoticons = YAML.load_file(file_path)
+  emotes = {}
+
+  emoticons.each do |key, value|
+    emotes[key] = {}
+  end
 end
 
-def get_japanese_emoticon
-
+def get_japanese_emoticon (file, emote)
+  file.each do |key|
+    if file[key][:japanese] = emote
+      return file[key]
+    end
+  end
 end
 
 def get_english_meaning
