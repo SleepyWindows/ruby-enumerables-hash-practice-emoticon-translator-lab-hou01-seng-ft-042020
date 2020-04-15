411421b26f92dd28722463cc36724ebc66e4e326
edwin calvillo <calvillo.edwin@gmail.com>
2020-04-15 22:42:12 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require 'yaml'
 
 
@@ -12,7 +13,7 @@ def load_library (file)
   end
   emotes
 end
-
+binding.pry
 def get_japanese_emoticon (file, emote)
   library = load_library(file)
   library.each do |key|
