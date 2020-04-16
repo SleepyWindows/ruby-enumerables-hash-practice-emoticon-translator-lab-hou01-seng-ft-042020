242360d1534f02e112e384b3fbe8a4afd0f6e693
edwin calvillo <calvillo.edwin@gmail.com>
2020-04-16 00:07:48 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,7 +19,7 @@ def get_japanese_emoticon (file, emote)
   library.each do |key, value|
     #binding.pry
     if value[:english] == emote
-      binding.pry
+      
       return value[:japanese]
     else
       "Sorry, that emoticon was not found"
